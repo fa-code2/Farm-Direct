a7e0111ec2da5b0127b06f0f5d5071b587ee1ed2
Fatima Aslam <faaa367810@gmail.com>
2025-04-05 03:54:28 +0530

    Update landing_page.jsx
     Update the "Farmer Login" Button in Landing_page

--- src/landing_page.jsx
+++ src/landing_page.jsx
@@ -1,6 +1,9 @@
-import React from 'react'
+import React from 'react';
+import { useNavigate } from 'react-router-dom';
 
 const Landing_page = () => {
+  const navigate = useNavigate();
+
   return (
     <>
     
